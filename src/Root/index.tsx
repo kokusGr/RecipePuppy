2c24a6d886463b827ef83263d8543c82cf1ff3d5
kokus <gren.konrad@gmail.com>
2019-08-07 19:06:37 +0200

    Add basic search bar

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -2,21 +2,53 @@ import React, { Fragment } from 'react'
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
+  View,
+  TextInput,
   StatusBar,
+  Alert,
 } from 'react-native'
 
 const App = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Start</Text>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.search}
+            onSubmitEditing={() => {
+              Alert.alert('On submit editing')
+            }}
+            placeholder="onion, carrot"
+            placeholderTextColor="gray"
+            selectionColor="green"
+            returnKeyType="search"
+            autoCapitalize="none"
+            autoCompleteType="off"
+            autoCorrect={false}
+            autoFocus={true}
+          />
+        </View>
       </SafeAreaView>
     </Fragment>
   );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 8,
+    paddingTop: 20,
+  },
+  search: {
+    fontSize: 16,
+    borderWidth: 1,
+    height: 40,
+    paddingHorizontal: 16,
+  },
+})
 
 export default App
